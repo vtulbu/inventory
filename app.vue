b5bbaf135573a12f6308b7534f6bf5b46a8b6139
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-09-25 18:04:40 +0300

    Create index and inventory pages

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <div class="h-screen flex justify-center items-center bg-gray-200">Hello</div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+body {
+  min-height: 100vh;
+}
+</style>
